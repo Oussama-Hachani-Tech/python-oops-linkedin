203dfe03353f863fd92a009d6e3c7500d4c6877c
Oussama Hachani <OHachani.tech@Outlook.com>
2024-04-10 02:43:08 +0100

    feature(chapter1): basic class definition

--- Start/Ch 1/definition_start.py
+++ Start/Ch 1/definition_start.py
@@ -3,9 +3,18 @@
 
 
 # TODO: create a basic class
+class Book:
+    def __new__(cls, *args, **kwargs):
+        return super(Book, cls).__new__(cls)
+
+    def __init__(self, title):
+        self.title = title
 
 
 # TODO: create instances of the class
-
+quran = Book("Holy Quran")
+two_cities = Book("A Tale of Two Cities")
 
 # TODO: print the class and property
+print(quran)
+print(quran.title)
